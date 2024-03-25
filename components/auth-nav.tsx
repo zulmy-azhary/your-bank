b1a17bb7d34b3dcea34e80aa7e2881bc59280fcf
zoel-a <zulmyazhary32@gmail.com>
2024-03-26 04:44:47 +0800

    fix: onClose sidebar for auth nav

--- components/auth-nav.tsx
+++ components/auth-nav.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Reveal } from "@/components/utils/reveal";
+import { useToggle } from "@/context/toggle";
 import Link from "next/link";
 
 export const AuthNav: React.FC = () => {
+  const { onClose } = useToggle();
+
   return (
     <div className="flex flex-col lg:flex-row gap-y-8 w-full lg:w-fit items-center gap-x-1">
       <Link
         href="/signup"
+        onClick={onClose}
         className="text-center lg:text-start text-sm 2xl:text-lg px-[18px] py-[10px] 2xl:py-3 2xl:px-6 rounded-full transition-colors w-full hover:text-shades-green-60"
       >
         <Reveal delay={0.45} className="w-full">
           Sign Up
         </Reveal>
       </Link>
-      <Link href="/login" className="w-full lg:w-fit mx-auto">
+      <Link href="/login" onClick={onClose} className="w-full lg:w-fit mx-auto">
         <Reveal delay={0.55} className="w-full">
           <Button className="w-full">Login</Button>
         </Reveal>
