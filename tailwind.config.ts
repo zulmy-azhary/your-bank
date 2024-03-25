57d74ce1601b9b53e73613b1839cb4e41e23fa85
zoel-a <zulmyazhary32@gmail.com>
2024-03-26 01:04:27 +0800

    chore: setting theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,46 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        shades: {
+          green: {
+            60: "#CAFF33",
+            65: "#D1FF4C",
+            70: "#D8FF66",
+            80: "#E5FF99",
+            90: "#F2FFCC",
+            95: "#F8FFE5",
+            97: "#FBFFF0",
+            99: "#FEFFFA",
+          },
+          white: {
+            90: "#E4E4E7",
+            95: "#F1F1F3",
+            97: "#F7F7F8",
+            99: "#FCFCFD",
+          },
+          grey: {
+            10: "#191919",
+            11: "#1C1C1C",
+            15: "#262626",
+            20: "#333333",
+            30: "#4C4C4D",
+            35: "#59595A",
+            40: "#656567",
+            60: "#98989A",
+            70: "#B3B3B3",
+            75: "#BFBFBF",
+          },
+        },
       },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      "2xl": "1920px",
+      "3xl": "2560px",
     },
   },
   plugins: [],
