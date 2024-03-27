6fa1b24586c5d424aee4089c965a17ea2f57bd97
zoel-a <zulmyazhary32@gmail.com>
2024-03-27 16:03:06 +0800

    feat: add hero section

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
+import { Hero } from "./_components/sections/hero/hero-section";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <main className="w-full overflow-x-hidden px-4 pb-8 lg:px-20 2xl:px-[162px] xl:container lg:py-10">
+      <Hero />
+    </main>
+  );
 }
