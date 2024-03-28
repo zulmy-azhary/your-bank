095db637db4d748a9f0c950b4ec58d47bae69864
zoel-a <zulmyazhary32@gmail.com>
2024-03-28 17:21:45 +0800

    refactor: rename hero section

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Hero } from "./_components/sections/hero/hero-section";
+import { Hero } from "./_components/sections/hero";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-    <main className="w-full overflow-x-hidden px-4 pb-8 lg:px-20 2xl:px-[162px] xl:container lg:py-10">
+    <main className="w-full overflow-x-hidden flex flex-col px-4 pb-8 lg:px-20 2xl:px-[162px] xl:container lg:py-10 gap-y-20 lg:gap-y-28 2xl:gap-y-32">
       <Hero />
     </main>
   );
