d4259c100070102c368e179a95c96febda883ec9
zoel-a <zulmyazhary32@gmail.com>
2024-03-31 06:16:53 +0800

    feat: create features' tabs

---
+++ app/(index)/_components/features/features-tabs.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+const tabs = ["Outline Banking", "Financial Tools", "Customer Support"] as const;
+
+export const FeaturesTabs: React.FC = () => {
+  const [selected, setSelected] = useState<(typeof tabs)[number]>(tabs[0]);
+
+  return (
+    <div className=" overflow-x-auto modified-scrollbar h-fit rounded-[10px] 2xl:rounded-xl bg-shades-grey-11 p-5 lg:p-10 2xl:p-[50px] flex flex-nowrap lg:flex-col gap-x-5 gap-y-6">
+      {tabs.map((tab) => (
+        <Button
+          key={tab}
+          onClick={() => setSelected(tab)}
+          variant="secondary"
+          className={cn(
+            "lg:w-full md:grow shrink-0",
+            selected === tab
+              ? "text-shades-green-60 bg-shades-grey-10 hover:bg-shades-grey-10"
+              : null
+          )}
+        >
+          {tab}
+        </Button>
+      ))}
+    </div>
+  );
+};
