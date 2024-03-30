30c265c3028e4299db5b7b90a1cf2f397f1d761f
zoel-a <zulmyazhary32@gmail.com>
2024-03-30 18:57:42 +0800

    refactor: rename useState's variable name

--- app/(index)/_components/products/products-tabs.tsx
+++ app/(index)/_components/products/products-tabs.tsx
@@ -6,15 +6,15 @@ import { useState } from "react";
 const tabs = ["For Individuals", "For Businesses"] as const;
 
 export const ProductsTabs: React.FC = () => {
-  const [isActive, setActive] = useState<(typeof tabs)[number]>(tabs[0]);
+  const [selected, setSelected] = useState<(typeof tabs)[number]>(tabs[0]);
 
   return (
     <div className="w-fit flex bg-shades-grey-11 border border-shades-grey-15 rounded-full p-3">
       {tabs.map((tab) => (
         <Button
           key={tab}
-          onClick={() => setActive(tab)}
-          variant={isActive === tab ? "primary" : "ghost"}
+          onClick={() => setSelected(tab)}
+          variant={selected === tab ? "primary" : "ghost"}
         >
           {tab}
         </Button>
