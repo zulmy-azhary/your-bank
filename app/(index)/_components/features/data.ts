22bb034693b649e8f55c7714baa81304ff70eedd
zoel-a <zulmyazhary32@gmail.com>
2024-03-31 06:16:14 +0800

    feat: create features' card data

---
+++ app/(index)/_components/features/data.ts
@@ -0,0 +1,22 @@
+export const features = [
+  {
+    title: "24/7 Account Access",
+    description:
+      "Enjoy the convenience of accessing your accounts anytime, anywhere through our secure online banking platform. Check balances, transfer funds, and pay bills with ease.",
+  },
+  {
+    title: "Mobile Banking App",
+    description:
+      "Stay connected to your finances on the go with our user-friendly mobile banking app. Easily manage your accounts, deposit checks, and make payments from your smartphone or tablet.",
+  },
+  {
+    title: "Secure Transactions",
+    description:
+      "Rest assured knowing that your transactions are protected by industry-leading security measures. We employ encryption and multi-factor authentication to safeguard your financial information.",
+  },
+  {
+    title: "Bill Pay and Transfers",
+    description:
+      "Save time and avoid late fees with our convenient bill pay service. Set up recurring payments or make one-time transfers between your accounts with just a few clicks.",
+  },
+];
