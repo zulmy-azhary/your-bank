b970afff3110c65231cf9995a8e6fc52e65367d6
zoel-a <zulmyazhary32@gmail.com>
2024-03-30 16:16:57 +0800

    feat: add use-cases' section, update hero & products path

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { Hero } from "./_components/sections/hero";
-import { Products } from "./_components/sections/products";
+import { Hero } from "./_components/hero";
+import { Products } from "./_components/products";
+import { UseCases } from "./_components/use-cases";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -11,6 +12,7 @@ export default function HomePage() {
     <main className="w-full overflow-x-hidden flex flex-col px-4 pb-8 lg:px-20 2xl:px-[162px] xl:container lg:py-10 gap-y-20 lg:gap-y-28 2xl:gap-y-32">
       <Hero />
       <Products />
+      <UseCases />
     </main>
   );
 }
