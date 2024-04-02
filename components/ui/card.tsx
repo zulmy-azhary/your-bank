5b829eb55684d93d0b21f8e7a00767c46744e662
zoel-a <zulmyazhary32@gmail.com>
2024-04-03 06:05:47 +0800

    style: update rounded for card component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ export const Card: React.FC<CardProps> = (props) => {
   return (
     <div
       className={cn(
-        "min-w-60 min-h-80 bg-[#1A1A1A] border border-shades-grey-15 rounded-md p-[21px] lg:p-7 2xl:p-[35px] relative",
+        "min-w-60 min-h-80 bg-[#1A1A1A] border border-shades-grey-15 rounded-md md:rounded-lg 2xl:rounded-[10.4px] p-[21px] lg:p-7 2xl:p-[35px] relative",
         className
       )}
       {...rest}
