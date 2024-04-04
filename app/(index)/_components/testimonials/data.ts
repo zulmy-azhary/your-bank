c4caca0799afeacba901c7633aefecd0acfec2df
zoel-a <zulmyazhary32@gmail.com>
2024-04-05 00:02:14 +0800

    feat: create testimonials' data

---
+++ app/(index)/_components/testimonials/data.ts
@@ -0,0 +1,17 @@
+export const testimonials = [
+  {
+    user: "Sara T",
+    comment:
+      "YourBank has been my trusted financial partner for years. Their personalized service and innovative digital banking solutions have made managing my finances a breeze.",
+  },
+  {
+    user: "John D",
+    comment:
+      "I recently started my own business, and YourBank has been instrumental in helping me set up my business accounts and secure the financing I needed. Their expert guidance and tailored solutions have been invaluable.",
+  },
+  {
+    user: "Emily G",
+    comment:
+      "I love the convenience of YourBank's mobile banking app. It allows me to stay on top of my finances and make transactions on the go. The app is user-friendly and secure, giving me peace of mind.",
+  },
+];
