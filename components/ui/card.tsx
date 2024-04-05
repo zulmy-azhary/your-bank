3cbb63a5ef29be935ec1798584b58225643a20fc
zoel-a <zulmyazhary32@gmail.com>
2024-04-05 23:41:29 +0800

    style: remove min width & min height of card component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -4,10 +4,11 @@ type CardProps = React.ComponentProps<"div">;
 
 export const Card: React.FC<CardProps> = (props) => {
   const { className, children, ...rest } = props;
+
   return (
     <div
       className={cn(
-        "min-w-60 min-h-80 bg-[#1A1A1A] border border-shades-grey-15 rounded-md md:rounded-lg 2xl:rounded-[10.4px] p-[21px] lg:p-7 2xl:p-[35px] relative",
+        "bg-[#1A1A1A] border border-shades-grey-15 rounded-md md:rounded-lg 2xl:rounded-[10.4px] p-[21px] lg:p-7 2xl:p-[35px] relative",
         className
       )}
       {...rest}
