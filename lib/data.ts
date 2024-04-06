2736cbf2eb35de307d387d534c1bf02958118106
zoel-a <zulmyazhary32@gmail.com>
2024-04-07 02:06:40 +0800

    feat: create routes navigation's data

---
+++ lib/data.ts
@@ -0,0 +1,18 @@
+export const routes = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/careers",
+    label: "Careers",
+  },
+  {
+    href: "/about",
+    label: "About",
+  },
+  {
+    href: "/security",
+    label: "Security",
+  },
+];
