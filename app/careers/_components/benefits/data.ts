65b63c79b0589fa04e31312645569a245dc34da2
zoel-a <zulmyazhary32@gmail.com>
2024-04-08 16:50:45 +0800

    feat: create benefits' data

---
+++ app/careers/_components/benefits/data.ts
@@ -0,0 +1,33 @@
+import {
+  HiBriefcase,
+  HiInboxStack,
+  HiLightBulb,
+  HiMiniPresentationChartBar,
+} from "react-icons/hi2";
+
+export const benefits = [
+  {
+    icon: HiMiniPresentationChartBar,
+    title: "Competitive Compensation",
+    description:
+      "We provide a competitive salary package that recognizes the skills and expertise of our employees. YourBank believes in rewarding exceptional performance and offering opportunities for financial growth.",
+  },
+  {
+    icon: HiLightBulb,
+    title: "Health and Wellness",
+    description:
+      "We prioritize the health and well-being of our employees by providing comprehensive medical, dental, and vision insurance plans. We also offer wellness programs, gym memberships, and resources to support a healthy lifestyle.",
+  },
+  {
+    icon: HiBriefcase,
+    title: "Retirement Planning",
+    description:
+      "YourBank is committed to helping employees plan for their future. We offer a retirement savings plan with a generous employer match to help them build a secure financial foundation for the long term.",
+  },
+  {
+    icon: HiInboxStack,
+    title: "Work-Life Balance",
+    description:
+      "We understand the importance of maintaining a healthy work-life balance. YourBank offers flexible work arrangements, paid time off, parental leave, and other programs that support employees in managing their personal and professional commitments.",
+  },
+];
