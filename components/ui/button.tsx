716f664231df0fd814053cc0265736d2a428abd0
zoel-a <zulmyazhary32@gmail.com>
2024-04-09 16:38:55 +0800

    style: add button size "base" variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,27 +1,31 @@
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
-const buttonVariants = cva("rounded-full w-fit text-sm 2xl:text-lg transition-colors disabled:opacity-30", {
-  variants: {
-    variant: {
-      primary: "bg-shades-green-60 text-shades-grey-11 hover:bg-shades-green-60/90",
-      secondary:
-        "bg-shades-grey-11 text-white border border-shades-grey-15 hover:bg-shades-grey-15/90",
-      accent: "bg-[#22251B] text-shades-green-65 hover:bg-[#22251B]/90",
-      ghost: "bg-transparent text-white hover:text-white/90",
+const buttonVariants = cva(
+  "rounded-full w-fit text-sm 2xl:text-lg transition-colors disabled:opacity-30",
+  {
+    variants: {
+      variant: {
+        primary: "bg-shades-green-60 text-shades-grey-11 hover:bg-shades-green-60/90",
+        secondary:
+          "bg-shades-grey-11 text-white border border-shades-grey-15 hover:bg-shades-grey-15/90",
+        accent: "bg-[#22251B] text-shades-green-65 hover:bg-[#22251B]/90",
+        ghost: "bg-transparent text-white hover:text-white/90",
+      },
+      size: {
+        default: "px-[18px] py-2.5 2xl:px-6 2xl:py-3.5",
+        sm: "px-5 py-3.5 2xl:px-6 2xl:py-3.5",
+        base: "px-5 py-3 2xl:px-[30px] 2xl:py-4",
+        md: "px-6 py-3 2xl:px-[30px] 2xl:py-3.5",
+        lg: "px-6 py-3.5 2xl:px-[30px] 2xl:py-[18px]",
+      },
     },
-    size: {
-      default: "px-[18px] py-[10px] 2xl:px-[24px] 2xl:py-[14px]",
-      sm: "px-[20px] py-[14px] 2xl:px-[24px] 2xl:py-[14px]",
-      md: "px-[24px] py-[12px] 2xl:px-[30px] 2xl:py-[14px]",
-      lg: "px-[24px] py-[14px] 2xl:px-[30px] 2xl:py-[18px]",
+    defaultVariants: {
+      variant: "primary",
+      size: "default",
     },
-  },
-  defaultVariants: {
-    variant: "primary",
-    size: "default",
-  },
-});
+  }
+);
 
 type ButtonProps = React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>;
 
