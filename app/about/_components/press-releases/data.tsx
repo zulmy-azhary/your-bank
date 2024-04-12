a0c43484349bc3e2e8a7f83641db02cb18c82a8b
zoel-a <zulmyazhary32@gmail.com>
2024-04-12 16:55:06 +0800

    feat: create press releases' data

---
+++ app/about/_components/press-releases/data.tsx
@@ -0,0 +1,40 @@
+import releaseFirst from "@/public/assets/release-1.png";
+import releaseSecond from "@/public/assets/release-2.png";
+import releaseThird from "@/public/assets/release-3.png";
+import releaseFourth from "@/public/assets/release-4.png";
+
+export const pressReleases = [
+  {
+    image: releaseFirst,
+    title: "YourBank Launches New Rewards Program to Enhance Customer Loyalty and Satisfaction",
+    location: "Indonesia",
+    date: "2024-11-06",
+    description:
+      "YourBank is pleased to announce the introduction of our new Rewards Program, aimed at rewarding our loyal customers and enhancing their banking experience. The program offers exclusive benefits, discounts, and personalized offers tailored to individual customer preferences. With this initiative, YourBank reaffirms its commitment to delivering exceptional value and building lasting relationships with our valued customers.",
+  },
+  {
+    image: releaseSecond,
+    title: "YourBank Expands Branch Network with Opening of New Location in Chennai",
+    location: "Indonesia",
+    date: "2024-11-12",
+    description:
+      "YourBank is excited to announce the grand opening of our newest branch in [City]. This expansion is a testament to our continued commitment to serving our customers and providing them with convenient access to our comprehensive range of banking services. The new branch will feature state-of-the-art facilities, a team of dedicated professionals, and a personalized approach to banking, further strengthening our presence in the local community.",
+  },
+  {
+    image: releaseThird,
+    title: "YourBank Partners with Local Nonprofit to Support Financial Education Initiatives",
+    location: "Indonesia",
+    date: "2024-12-24",
+    description:
+      "YourBank is excited to unveil our new Sustainable Banking Initiative, demonstrating our commitment to environmental responsibility. This initiative includes a range of sustainable banking products and services, such as green loans, eco-friendly investment options, and paperless banking solutions. By incorporating sustainable practices into our operations, we aim to contribute to a greener future while providing innovative banking solutions to our customers.",
+  },
+  {
+    image: releaseFourth,
+    title:
+      "YourBank Launches Sustainable Banking Initiative to Promote Environmental Responsibility",
+    location: "Indonesia",
+    date: "2024-12-28",
+    description:
+      "YourBank is excited to unveil our new Sustainable Banking Initiative, demonstrating our commitment to environmental responsibility. This initiative includes a range of sustainable banking products and services, such as green loans, eco-friendly investment options, and paperless banking solutions. By incorporating sustainable practices into our operations, we aim to contribute to a greener future while providing innovative banking solutions to our customers.",
+  },
+];
