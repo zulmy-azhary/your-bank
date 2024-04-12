4a03a86160770445e0103e814fde3d9a44894e69
zoel-a <zulmyazhary32@gmail.com>
2024-04-12 16:04:21 +0800

    refactor: add badge's default component props

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,12 +1,20 @@
-type BadgeProps = {
+import { cn } from "@/lib/utils";
+
+type BadgeProps = React.ComponentProps<"div"> & {
   children: React.ReactNode;
 };
 
 export const Badge: React.FC<BadgeProps> = (props) => {
-  const { children } = props;
+  const { children, className, ...rest } = props;
 
   return (
-    <div className="px-2.5 lg:px-3.5 2xl:px-4 py-1.5 2xl:py-2 rounded-full bg-shades-grey-10 border border-shades-grey-15">
+    <div
+      className={cn(
+        "px-2.5 lg:px-3.5 2xl:px-4 py-1.5 2xl:py-2 rounded-full bg-shades-grey-10 border border-shades-grey-15",
+        className
+      )}
+      {...rest}
+    >
       <p className="font-light text-sm lg:text-base 2xl:text-lg text-shades-grey-70">{children}</p>
     </div>
   );
