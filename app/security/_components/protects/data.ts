e0570f3031c8a7113f1aeb922f6553eded27eed3
zoel-a <zulmyazhary32@gmail.com>
2024-04-16 22:27:28 +0800

    feat: create protects' data

---
+++ app/security/_components/protects/data.ts
@@ -0,0 +1,28 @@
+import { HiSquare3Stack3D, HiSquaresPlus, HiSparkles, HiCurrencyRupee } from "react-icons/hi2";
+
+export const protects = [
+  {
+    icon: HiSquaresPlus,
+    title: "Secure Online Banking Platform",
+    description:
+      "Our online banking platform is built with multiple layers of security to safeguard your information. We utilize industry-standard encryption protocols to ensure that your data remains confidential and protected during transmission.",
+  },
+  {
+    icon: HiSquare3Stack3D,
+    title: "Multi-Factor Authentication",
+    description:
+      "To enhance the security of your online banking experience, we employ multi-factor authentication. This additional layer of security requires you to provide multiple pieces of identification, such as a password and a one-time verification code, to access your account.",
+  },
+  {
+    icon: HiSparkles,
+    title: "Fraud Monitoring",
+    description:
+      "We have sophisticated fraud detection systems in place to monitor your accounts for any suspicious activities. Our dedicated team works around the clock to detect and prevent unauthorized transactions, providing you with peace of mind.",
+  },
+  {
+    icon: HiCurrencyRupee,
+    title: "Secure Mobile Banking",
+    description:
+      "Our mobile banking app is designed with the same level of security as our online banking platform. You can confidently access your accounts, make transactions, and manage your finances on the go, knowing that your information is protected.",
+  },
+];
