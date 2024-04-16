b1408f9cb688418809ef08a95cab7757450b6785
zoel-a <zulmyazhary32@gmail.com>
2024-04-16 22:28:52 +0800

    feat: create security section component & its sub components

--- app/security/page.tsx
+++ app/security/page.tsx
@@ -1,6 +1,7 @@
-import { FAQS } from "@/components/sections/faqs";
 import type { Metadata } from "next";
 import { Overview } from "./_components/overview";
+import { Protects } from "./_components/protects";
+import { FAQS } from "@/components/sections/faqs";
 
 export const metadata: Metadata = {
   title: "Security",
@@ -10,6 +11,7 @@ export default function SecurityPage() {
   return (
     <>
       <Overview />
+      <Protects />
       <FAQS />
     </>
   );
