acae5e3319c5a7e5e6f73a70a7d5c9dc4e11d9c8
zoel-a <zulmyazhary32@gmail.com>
2024-04-23 14:35:26 +0800

    feat: add animation for faqs' section

--- components/sections/faqs/index.tsx
+++ components/sections/faqs/index.tsx
@@ -3,16 +3,21 @@ import { Section } from "@/components/ui/section";
 import { HiMiniChevronDown } from "react-icons/hi2";
 import { faqs } from "./data";
 import { FaqCard } from "./faq-card";
+import { Reveal } from "@/components/utils/reveal";
 
 const FaqsTitle: React.FC = () => {
   return (
     <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-      <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
-        <span className="text-shades-green-60">Frequently</span> Asked Questions
-      </h2>
-      <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-        Still you have any questions? Contact our Team via support@yourbank.com
-      </p>
+      <Reveal wrapperClassName="w-full">
+        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
+          <span className="text-shades-green-60">Frequently</span> Asked Questions
+        </h2>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+          Still you have any questions? Contact our Team via support@yourbank.com
+        </p>
+      </Reveal>
     </div>
   );
 };
@@ -21,13 +26,17 @@ const FaqsContent: React.FC = () => {
   return (
     <div className="flex flex-col items-center relative after:absolute after:inset-x-0 after:h-1/3 after:bottom-0 after:bg-gradient-to-t after:from-shades-grey-10 after:from-25% md:after:from-50% after:to-transparent after:pointer-events-none">
       <div className="w-full grid md:grid-cols-2 md:grid-rows-2 gap-5 lg:gap-[30px]">
-        {faqs.map(({ question, answer }) => (
-          <FaqCard key={question} question={question} answer={answer} />
+        {faqs.map(({ question, answer }, idx) => (
+          <Reveal key={question} delay={(idx + 1) * 0.25} className="h-full">
+            <FaqCard question={question} answer={answer} />
+          </Reveal>
         ))}
       </div>
-      <Button variant="secondary" className="flex items-center gap-x-1 z-10">
-        Load All FAQ&apos;s <HiMiniChevronDown className="size-6" />
-      </Button>
+      <Reveal wrapperClassName="z-10">
+        <Button variant="secondary" className="flex items-center gap-x-1">
+          Load All FAQ&apos;s <HiMiniChevronDown className="size-6" />
+        </Button>
+      </Reveal>
     </div>
   );
 };
