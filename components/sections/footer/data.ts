2668885e2e95fbcc39d75924e63997e32898ee39
zoel-a <zulmyazhary32@gmail.com>
2024-04-23 21:47:45 +0800

    feat: create data for footer section

---
+++ components/sections/footer/data.ts
@@ -0,0 +1,35 @@
+import { BiLogoFacebookCircle, BiLogoLinkedinSquare, BiLogoTwitter } from "react-icons/bi";
+import { HiEnvelope, HiMapPin, HiPhone } from "react-icons/hi2";
+
+export const contacts = [
+  {
+    href: "#",
+    icon: HiEnvelope,
+    text: "hello@yourbank.com",
+  },
+  {
+    href: "#",
+    icon: HiPhone,
+    text: "+62 81 234 567 890",
+  },
+  {
+    href: "#",
+    icon: HiMapPin,
+    text: "Makassar, Indonesia",
+  },
+];
+
+export const socials = [
+  {
+    href: "#",
+    icon: BiLogoFacebookCircle,
+  },
+  {
+    href: "#",
+    icon: BiLogoTwitter,
+  },
+  {
+    href: "#",
+    icon: BiLogoLinkedinSquare,
+  },
+];
