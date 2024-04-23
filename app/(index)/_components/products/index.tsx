d0227bb522c7738f364e165f25b60aed7ea0b1e9
zoel-a <zulmyazhary32@gmail.com>
2024-04-23 11:08:08 +0800

    feat: add animation for products' section

--- app/(index)/_components/products/index.tsx
+++ app/(index)/_components/products/index.tsx
@@ -3,18 +3,23 @@ import { ProductsItem } from "./products-item";
 import { ProductsTabs } from "./products-tabs";
 import { products } from "./data";
 import { Fragment } from "react";
+import { Reveal } from "@/components/utils/reveal";
 
 const ProductsTitle: React.FC = () => {
   return (
     <div className="flex flex-col items-center gap-y-5 md:flex-row md:justify-between">
       <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
-          Our <span className="text-shades-green-60">Products</span>
-        </h2>
-        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-          Discover a range of comprehensive and customizable banking products at YourBank, designed
-          to suit your unique financial needs and aspirations
-        </p>
+        <Reveal wrapperClassName="w-full">
+          <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
+            Our <span className="text-shades-green-60">Products</span>
+          </h2>
+        </Reveal>
+        <Reveal delay={0.35}>
+          <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+            Discover a range of comprehensive and customizable banking products at YourBank,
+            designed to suit your unique financial needs and aspirations
+          </p>
+        </Reveal>
       </div>
       <div className="md:self-end">
         <ProductsTabs />
@@ -26,17 +31,23 @@ const ProductsTitle: React.FC = () => {
 const ProductsContent: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row gap-y-[30px] lg:gap-x-4 xl:gap-x-10">
-      {products.map((product) => {
+      {products.map((product, idx) => {
         const { icon, title, description } = product;
         const isLastElement = product === products[products.length - 1];
 
         return (
           <Fragment key={title}>
-            <ProductsItem icon={icon} title={title} description={description} />
+            <Reveal delay={(idx + 1) * 0.075}>
+              <ProductsItem icon={icon} title={title} description={description} />
+            </Reveal>
             {isLastElement ? null : (
-              <div>
+              <Reveal
+                delay={(idx + 1) * 0.08}
+                wrapperClassName="overflow-visible"
+                className="h-full"
+              >
                 <span className="relative block w-full h-px lg:w-px lg:h-full bg-shades-grey-15" />
-              </div>
+              </Reveal>
             )}
           </Fragment>
         );
--- app/(index)/_components/products/products-tabs.tsx
+++ app/(index)/_components/products/products-tabs.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Appear } from "@/components/utils/appear";
+import { Reveal } from "@/components/utils/reveal";
 import { useState } from "react";
 
 const tabs = ["For Individuals", "For Businesses"] as const;
@@ -9,16 +11,14 @@ export const ProductsTabs: React.FC = () => {
   const [selected, setSelected] = useState<(typeof tabs)[number]>(tabs[0]);
 
   return (
-    <div className="w-fit flex bg-shades-grey-11 border border-shades-grey-15 rounded-full p-3">
-      {tabs.map((tab) => (
-        <Button
-          key={tab}
-          onClick={() => setSelected(tab)}
-          variant={selected === tab ? "primary" : "ghost"}
-        >
-          {tab}
-        </Button>
+    <Appear className="w-fit flex bg-shades-grey-11 border border-shades-grey-15 rounded-full p-3">
+      {tabs.map((tab, idx) => (
+        <Reveal key={tab} delay={(idx + 1) * 0.1}>
+          <Button onClick={() => setSelected(tab)} variant={selected === tab ? "primary" : "ghost"}>
+            {tab}
+          </Button>
+        </Reveal>
       ))}
-    </div>
+    </Appear>
   );
 };
