7f170b811fd0cce017044f90cdc66d384b72085a
zoel-a <zulmyazhary32@gmail.com>
2024-04-24 12:54:30 +0800

    feat: add animation on protects' section

--- app/security/_components/protects/index.tsx
+++ app/security/_components/protects/index.tsx
@@ -2,25 +2,31 @@ import { Section } from "@/components/ui/section";
 import { protects } from "./data";
 import { ProtectCard } from "./protect-card";
 import { cn } from "@/lib/utils";
+import { Reveal } from "@/components/utils/reveal";
+import { Appear } from "@/components/utils/appear";
 
 const ProtectsTitle: React.FC = () => {
   return (
     <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-      <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
-        How We <span className="text-shades-green-60">Protect You</span>
-      </h2>
-      <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-        At YourBank, we prioritize the security and confidentiality of your financial information.
-        Our state-of-the-art encryption technology and stringent data protection measures ensure
-        your assets and transactions are safeguarded at all times
-      </p>
+      <Reveal wrapperClassName="w-full">
+        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
+          How We <span className="text-shades-green-60">Protect You</span>
+        </h2>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+          At YourBank, we prioritize the security and confidentiality of your financial information.
+          Our state-of-the-art encryption technology and stringent data protection measures ensure
+          your assets and transactions are safeguarded at all times
+        </p>
+      </Reveal>
     </div>
   );
 };
 
 const ProtectsContent: React.FC = () => {
   return (
-    <div
+    <Appear
       className={cn(
         "grid md:grid-cols-2 gap-5 2xl:gap-[30px] xl:px-20 2xl:px-[100px] relative",
         "xl:-mt-16 xl:pt-16 2xl:-mt-28"
@@ -32,12 +38,11 @@ const ProtectsContent: React.FC = () => {
           "xl:before:absolute xl:before:inset-0 xl:before:w-full xl:before:h-full xl:before:bg-gradient-to-br xl:before:from-shades-green-60/5 xl:before:to-transparent xl:before:to-30% xl:before:rounded-t-[50px] xl:before:rounded-b-[20px]",
           "xl:after:absolute xl:after:inset-0 xl:after:bg-gradient-to-bl xl:after:from-shades-grey-11 xl:after:to-transparent xl:after:to-80% xl:after:rounded-t-[50px] xl:after:rounded-b-[20px]"
         )}
-      >
-      </div>
+      ></div>
       {protects.map((protect) => (
         <ProtectCard key={protect.title} {...protect} />
       ))}
-    </div>
+    </Appear>
   );
 };
 
--- app/security/_components/protects/protect-card.tsx
+++ app/security/_components/protects/protect-card.tsx
@@ -1,27 +1,32 @@
 import { Icon } from "@/components/ui/icon";
 import { protects } from "./data";
 import { cn } from "@/lib/utils";
+import { Reveal } from "@/components/utils/reveal";
 
 type ProtectCardProps = (typeof protects)[number] & React.ComponentPropsWithoutRef<"div">;
 
 export const ProtectCard: React.FC<ProtectCardProps> = (props) => {
-  const { icon, title, description, className, ...rest } = props;
+  const { icon, title, description, className } = props;
+
   return (
-    <div
-      key={title}
-      className={cn(
-        "rounded-[20px] bg-shades-grey-11 border border-shades-grey-15 p-6 lg:p-10 2xl:p-[50px] space-y-[18px] lg:space-y-5 2xl:space-y-6 z-10",
+    <Reveal
+      wrapperClassName={cn(
+        "rounded-[20px] bg-shades-grey-11 border border-shades-grey-15 p-6 lg:p-10 2xl:p-[50px] z-10",
         className
       )}
-      {...rest}
+      className="h-full space-y-[18px] lg:space-y-5 2xl:space-y-6"
     >
       <div className="flex items-center gap-x-3.5 2xl:gap-x-5">
         <Icon size="sm" icon={icon} />
-        <h3 className="text-lg lg:text-xl 2xl:text-2xl">{title}</h3>
+        <Reveal>
+          <h3 className="text-lg lg:text-xl 2xl:text-2xl">{title}</h3>
+        </Reveal>
       </div>
-      <p className="font-light text-shades-grey-70 text-sm lg:text-base 2xl:text-lg">
-        {description}
-      </p>
-    </div>
+      <Reveal delay={0.35}>
+        <p className="font-light text-shades-grey-70 text-sm lg:text-base 2xl:text-lg">
+          {description}
+        </p>
+      </Reveal>
+    </Reveal>
   );
 };
