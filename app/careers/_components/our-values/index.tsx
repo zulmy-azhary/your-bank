2cb37b174e77f73feaebbacc09a09434837c2cb1
zoel-a <zulmyazhary32@gmail.com>
2024-04-28 18:00:07 +0800

    feat: add animation for ourvalues' section

--- app/careers/_components/our-values/index.tsx
+++ app/careers/_components/our-values/index.tsx
@@ -1,19 +1,24 @@
 import { Section } from "@/components/ui/section";
 import { ourValues } from "./data";
 import { OurValuesCard } from "./ourvalues-card";
+import { Reveal } from "@/components/utils/reveal";
 
 const OurValuesTitle: React.FC = () => {
   return (
     <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-      <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
-        Our <span className="text-shades-green-60">Values</span>
-      </h2>
-      <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-        At YourBank, our values form the foundation of our organization and guide our actions. We
-        believe in upholding the highest standards of integrity, delivering exceptional service, and
-        embracing innovation. These values define our culture and shape the way we work together to
-        achieve our goals.
-      </p>
+      <Reveal wrapperClassName="w-full">
+        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
+          Our <span className="text-shades-green-60">Values</span>
+        </h2>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+          At YourBank, our values form the foundation of our organization and guide our actions. We
+          believe in upholding the highest standards of integrity, delivering exceptional service,
+          and embracing innovation. These values define our culture and shape the way we work
+          together to achieve our goals.
+        </p>
+      </Reveal>
     </div>
   );
 };
@@ -21,8 +26,10 @@ const OurValuesTitle: React.FC = () => {
 const OurValuesContent: React.FC = () => {
   return (
     <div className="grid md:grid-cols-2 gap-[50px] lg:gap-[60px] 2xl:gap-20">
-      {ourValues.map(({ title, description }) => (
-        <OurValuesCard key={title} title={title} description={description} />
+      {ourValues.map(({ title, description }, idx) => (
+        <Reveal key={title} className="h-full" delay={(idx + 1) * 0.1}>
+          <OurValuesCard title={title} description={description} />
+        </Reveal>
       ))}
     </div>
   );
--- app/careers/_components/our-values/ourvalues-card.tsx
+++ app/careers/_components/our-values/ourvalues-card.tsx
@@ -1,3 +1,5 @@
+import { Reveal } from "@/components/utils/reveal";
+
 type OurValuesCardProps = {
   title: string;
   description: string;
@@ -7,13 +9,17 @@ export const OurValuesCard: React.FC<OurValuesCardProps> = (props) => {
   const { title, description } = props;
 
   return (
-    <div className="pl-5 lg:pl-6 2xl:pl-[30px] space-y-3.5 lg:space-y-5 2xl:space-y-[30px] border-l border-shades-green-60">
-      <h3 className="font-medium text-3xl xl:text-[40px] 2xl:text-[58px] text-shades-grey-30">
-        {title}
-      </h3>
-      <p className="text-sm xl:text-base 2xl:text-lg font-light text-shades-grey-70">
-        {description}
-      </p>
+    <div className="h-full pl-5 lg:pl-6 2xl:pl-[30px] space-y-3.5 lg:space-y-5 2xl:space-y-[30px] border-l border-shades-green-60">
+      <Reveal from="left">
+        <h3 className="font-medium text-3xl xl:text-[40px] 2xl:text-[58px] text-shades-grey-30">
+          {title}
+        </h3>
+      </Reveal>
+      <Reveal from="left" delay={0.35}>
+        <p className="text-sm xl:text-base 2xl:text-lg font-light text-shades-grey-70">
+          {description}
+        </p>
+      </Reveal>
     </div>
   );
 };
