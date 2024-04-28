27592d751766d8a4bb6fb32627a76fe7fb30e2cf
zoel-a <zulmyazhary32@gmail.com>
2024-04-28 18:45:22 +0800

    feat: add animation for benefits' section

--- app/careers/_components/benefits/benefit-card.tsx
+++ app/careers/_components/benefits/benefit-card.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@/components/ui/icon";
+import { Reveal } from "@/components/utils/reveal";
 import type { IconType } from "react-icons";
 
 type BenefitCardProps = {
@@ -11,14 +12,20 @@ export const BenefitCard: React.FC<BenefitCardProps> = (props) => {
   const { icon, title, description } = props;
 
   return (
-    <div className="space-y-5 lg:space-y-6 2xl:space-y-[30px] p-6 lg:p-10 2xl:p-[50px] bg-shades-grey-11 border border-shades-grey-15 rounded-t-[40px] rounded-b-[14px] md:rounded-bl-[40px] md:rounded-tl-[14px] 2xl:rounded-tr-[50px] 2xl:rounded-bl-[50px] 2xl:rounded-tl-[20px] 2xl:rounded-br-[20px]">
+    <div className="h-full space-y-5 lg:space-y-6 2xl:space-y-[30px] p-6 lg:p-10 2xl:p-[50px] bg-shades-grey-11 border border-shades-grey-15 rounded-t-[40px] rounded-b-[14px] md:rounded-bl-[40px] md:rounded-tl-[14px] 2xl:rounded-tr-[50px] 2xl:rounded-bl-[50px] 2xl:rounded-tl-[20px] 2xl:rounded-br-[20px]">
       <div className="flex items-center gap-x-3.5 2xl:gap-x-5">
-        <Icon icon={icon} />
-        <h3 className="text-lg lg:text-xl 2xl:text-2xl">{title}</h3>
+        <Reveal wrapperClassName="z-10 shrink-0">
+          <Icon icon={icon} />
+        </Reveal>
+        <Reveal from="left" delay={0.35}>
+          <h3 className="text-lg lg:text-xl 2xl:text-2xl">{title}</h3>
+        </Reveal>
       </div>
-      <p className="text-sm lg:text-base 2xl:text-lg font-light text-shades-grey-70">
-        {description}
-      </p>
+      <Reveal delay={0.4}>
+        <p className="text-sm lg:text-base 2xl:text-lg font-light text-shades-grey-70">
+          {description}
+        </p>
+      </Reveal>
     </div>
   );
 };
--- app/careers/_components/benefits/index.tsx
+++ app/careers/_components/benefits/index.tsx
@@ -1,18 +1,23 @@
 import { Section } from "@/components/ui/section";
 import { benefits } from "./data";
 import { BenefitCard } from "./benefit-card";
+import { Reveal } from "@/components/utils/reveal";
 
 const BenefitsTitle: React.FC = () => {
   return (
     <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-      <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
-        Our <span className="text-shades-green-60">Benefits</span>
-      </h2>
-      <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-        At YourBank, we value our employees and are dedicated to their well-being and success. We
-        offer a comprehensive range of benefits designed to support their personal and professional
-        growth.
-      </p>
+      <Reveal wrapperClassName="w-full">
+        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium">
+          Our <span className="text-shades-green-60">Benefits</span>
+        </h2>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+          At YourBank, we value our employees and are dedicated to their well-being and success. We
+          offer a comprehensive range of benefits designed to support their personal and
+          professional growth.
+        </p>
+      </Reveal>
     </div>
   );
 };
@@ -20,13 +25,14 @@ const BenefitsTitle: React.FC = () => {
 const BenefitsContent: React.FC = () => {
   return (
     <div className="grid md:grid-cols-2 gap-6 lg:gap-[30px] 2xl:gap-[50px]">
-      {benefits.map((benefit) => (
-        <BenefitCard
-          key={benefit.title}
-          icon={benefit.icon}
-          title={benefit.title}
-          description={benefit.description}
-        />
+      {benefits.map((benefit, idx) => (
+        <Reveal key={benefit.title} className="h-full" delay={(idx + 1) * 0.1}>
+          <BenefitCard
+            icon={benefit.icon}
+            title={benefit.title}
+            description={benefit.description}
+          />
+        </Reveal>
       ))}
     </div>
   );
