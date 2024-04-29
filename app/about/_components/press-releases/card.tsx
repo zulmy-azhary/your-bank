d94d9bef1cb37d81c1a6cf8c0d7f8acef90ac412
zoel-a <zulmyazhary32@gmail.com>
2024-04-29 15:24:27 +0800

    feat: add animation for press releases' section

--- app/about/_components/press-releases/card.tsx
+++ app/about/_components/press-releases/card.tsx
@@ -3,6 +3,8 @@ import type { pressReleases } from "./data";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { Reveal } from "@/components/utils/reveal";
+import { Appear } from "@/components/utils/appear";
 
 type PressReleaseCardProps = React.ComponentProps<"div"> & {
   pressRelease: (typeof pressReleases)[number];
@@ -16,10 +18,16 @@ const PressReleaseHeader: React.FC<
 
   return (
     <div className="space-y-2.5 lg:space-y-3.5 2xl:space-y-5">
-      <h3 className="text-lg lg:text-xl 2xl:text-2xl">{title}</h3>
+      <Reveal>
+        <h3 className="text-lg lg:text-xl 2xl:text-2xl">{title}</h3>
+      </Reveal>
       <div className="flex flex-wrap gap-1.5 2xl:gap-2.5">
-        <Badge>Location: {location}</Badge>
-        <Badge>Date: {formattedDate}</Badge>
+        <Reveal delay={0.35}>
+          <Badge>Location: {location}</Badge>
+        </Reveal>
+        <Reveal delay={0.4}>
+          <Badge>Date: {formattedDate}</Badge>
+        </Reveal>
       </div>
     </div>
   );
@@ -32,23 +40,27 @@ export const PressReleaseCard: React.FC<PressReleaseCardProps> = (props) => {
   return (
     <div
       className={cn(
-        "bg-shades-grey-11 border border-shades-grey-15 p-5 lg:p-6 2xl:p-[30px] rounded-t-[40px] rounded-b-2xl space-y-[30px] lg:space-y-10 2xl:space-y-[51px]",
+        "h-full bg-shades-grey-11 border border-shades-grey-15 p-5 lg:p-6 2xl:p-[30px] rounded-t-[40px] rounded-b-2xl space-y-[30px] lg:space-y-10 2xl:space-y-[51px]",
         className
       )}
       {...rest}
     >
-      <Image
-        src={image}
-        alt={`${title} Image`}
-        placeholder="blur"
-        priority
-        className="object-cover grayscale rounded-t-[30px] rounded-b-xl max-h-[207px] lg:max-h-[277px] 2xl:max-h-[333px]"
-      />
+      <Appear delay={0.5}>
+        <Image
+          src={image}
+          alt={`${title} Image`}
+          placeholder="blur"
+          priority
+          className="object-cover grayscale rounded-t-[30px] rounded-b-xl max-h-[207px] lg:max-h-[277px] 2xl:max-h-[333px]"
+        />
+      </Appear>
       <div className="lg:px-6 2xl:px-[30px] space-y-6 2xl:space-y-[30px]">
         <PressReleaseHeader title={title} location={location} date={date} />
-        <p className="font-light text-shades-grey-70 text-sm lg:text-base 2xl:text-lg">
-          {description}
-        </p>
+        <Reveal delay={0.45}>
+          <p className="font-light text-shades-grey-70 text-sm lg:text-base 2xl:text-lg">
+            {description}
+          </p>
+        </Reveal>
       </div>
     </div>
   );
--- app/about/_components/press-releases/index.tsx
+++ app/about/_components/press-releases/index.tsx
@@ -1,17 +1,22 @@
 import { Section } from "@/components/ui/section";
 import { pressReleases } from "./data";
 import { PressReleaseCard } from "./card";
+import { Reveal } from "@/components/utils/reveal";
 
 const PressReleasesTitle: React.FC = () => {
   return (
     <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-      <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium text-shades-green-60">
-        Press Releases
-      </h2>
-      <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-        Stay updated with the latest happenings and exciting developments at YourBank through our
-        press releases.
-      </p>
+      <Reveal wrapperClassName="w-full">
+        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium text-shades-green-60">
+          Press Releases
+        </h2>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+          Stay updated with the latest happenings and exciting developments at YourBank through our
+          press releases.
+        </p>
+      </Reveal>
     </div>
   );
 };
@@ -19,8 +24,10 @@ const PressReleasesTitle: React.FC = () => {
 const PressReleasesContent: React.FC = () => {
   return (
     <div className="grid md:grid-cols-2 gap-5 2xl:gap-[30px]">
-      {pressReleases.map((pressRelease) => (
-        <PressReleaseCard key={pressRelease.title} pressRelease={pressRelease} />
+      {pressReleases.map((pressRelease, idx) => (
+        <Reveal key={pressRelease.title} delay={(idx + 1) * 0.1} className="h-full">
+          <PressReleaseCard pressRelease={pressRelease} />
+        </Reveal>
       ))}
     </div>
   );
