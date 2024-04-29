5de1d972b02d1a86e0b574ef76923730001ce1d2
zoel-a <zulmyazhary32@gmail.com>
2024-04-29 11:50:23 +0800

    feat: add animation for jobs' section

--- app/careers/_components/jobs/index.tsx
+++ app/careers/_components/jobs/index.tsx
@@ -1,18 +1,23 @@
 import { Section } from "@/components/ui/section";
 import { JobCard } from "./job-card";
 import { jobs } from "./data";
+import { Reveal } from "@/components/utils/reveal";
 
 const JobsTitle: React.FC = () => {
   return (
     <div className="space-y-2.5 text-center md:text-start md:basis-7/12">
-      <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium text-shades-green-60">
-        Job Openings
-      </h2>
-      <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
-        Explore exciting job openings at YourBank, where we value talent, innovation, and a passion
-        for customer service. Join our team and be part of shaping a brighter future in the banking
-        industry
-      </p>
+      <Reveal wrapperClassName="w-full">
+        <h2 className="text-[28px] lg:text-[38px] 2xl:text-5xl font-medium text-shades-green-60">
+          Job Openings
+        </h2>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="text-shades-grey-70 text-sm lg:text-base 2xl:text-lg leading-normal font-light">
+          Explore exciting job openings at YourBank, where we value talent, innovation, and a
+          passion for customer service. Join our team and be part of shaping a brighter future in
+          the banking industry
+        </p>
+      </Reveal>
     </div>
   );
 };
@@ -21,14 +26,15 @@ const JobsContent: React.FC = () => {
   return (
     <div className="grid md:grid-cols-2 gap-5 2xl:gap-[30px]">
       {jobs.map((job) => (
-        <JobCard
-          key={job.position}
-          position={job.position}
-          description={job.description}
-          location={job.location}
-          department={job.department}
-          requirements={job.requirements}
-        />
+        <Reveal key={job.position} className="h-full">
+          <JobCard
+            position={job.position}
+            description={job.description}
+            location={job.location}
+            department={job.department}
+            requirements={job.requirements}
+          />
+        </Reveal>
       ))}
     </div>
   );
--- app/careers/_components/jobs/job-card.tsx
+++ app/careers/_components/jobs/job-card.tsx
@@ -1,5 +1,6 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Reveal } from "@/components/utils/reveal";
 import { IoBriefcase } from "react-icons/io5";
 
 type JobCardProps = {
@@ -15,10 +16,16 @@ const JobHeader: React.FC<Pick<JobCardProps, "position" | "location" | "departme
 
   return (
     <div className="space-y-1.5 lg:space-y-2.5 2xl:space-y-3.5">
-      <h3 className="font-semibold text-xl lg:text-2xl 2xl:text-3xl">{position}</h3>
+      <Reveal>
+        <h3 className="font-semibold text-xl lg:text-2xl 2xl:text-3xl">{position}</h3>
+      </Reveal>
       <div className="flex flex-wrap gap-1.5 lg:gap-2 2xl:gap-2.5">
-        <Badge>Location: {location}</Badge>
-        <Badge>Department: {department}</Badge>
+        <Reveal delay={0.35}>
+          <Badge>Location: {location}</Badge>
+        </Reveal>
+        <Reveal delay={0.4}>
+          <Badge>Department: {department}</Badge>
+        </Reveal>
       </div>
     </div>
   );
@@ -29,10 +36,14 @@ const JobDescription: React.FC<Pick<JobCardProps, "description">> = (props) => {
 
   return (
     <div className="space-y-2 lg:space-y-4 2xl:space-y-5">
-      <h4 className="font-semibold text-lg lg:text-xl 2xl:text-2xl">About This Job</h4>
-      <p className="font-light text-shades-grey-70 text-sm lg:text-base 2xl:text-lg">
-        {description}
-      </p>
+      <Reveal>
+        <h4 className="font-semibold text-lg lg:text-xl 2xl:text-2xl">About This Job</h4>
+      </Reveal>
+      <Reveal delay={0.35}>
+        <p className="font-light text-shades-grey-70 text-sm lg:text-base 2xl:text-lg">
+          {description}
+        </p>
+      </Reveal>
     </div>
   );
 };
@@ -42,15 +53,21 @@ const JobRequirements: React.FC<Pick<JobCardProps, "requirements">> = (props) =>
 
   return (
     <div className="space-y-2.5 lg:space-y-4 2xl:space-y-5">
-      <h4 className="font-semibold text-lg lg:text-xl 2xl:text-2xl">
-        Requirements & Qualifications
-      </h4>
+      <Reveal>
+        <h4 className="font-semibold text-lg lg:text-xl 2xl:text-2xl">
+          Requirements & Qualifications
+        </h4>
+      </Reveal>
       <div className="space-y-3.5 2xl:space-y-5">
-        {requirements.map((requirement) => (
-          <div key={requirement} className="text-shades-grey-70 flex items-center gap-x-2">
+        {requirements.map((requirement, idx) => (
+          <Reveal
+            key={requirement}
+            delay={(idx + 1) * 0.1}
+            className="text-shades-grey-70 flex items-center gap-x-2"
+          >
             <IoBriefcase className="size-5 2xl:size-6 shrink-0" />
             <p className="font-light text-sm lg:text-base 2xl:text-lg">{requirement}</p>
-          </div>
+          </Reveal>
         ))}
       </div>
     </div>
@@ -61,7 +78,7 @@ export const JobCard: React.FC<JobCardProps> = (props) => {
   const { position, description, location, department, requirements } = props;
 
   return (
-    <div className="flex flex-col justify-between gap-y-[30px] lg:gap-y-10 2xl:gap-y-[50px] bg-shades-grey-11 border border-shades-grey-15 rounded-2xl p-6 lg:p-10 2xl:p-[50px]">
+    <div className="h-full flex flex-col justify-between gap-y-[30px] lg:gap-y-10 2xl:gap-y-[50px] bg-shades-grey-11 border border-shades-grey-15 rounded-2xl p-6 lg:p-10 2xl:p-[50px]">
       <div className="space-y-[30px] lg:space-y-10 2xl:space-y-[50px]">
         <JobHeader position={position} location={location} department={department} />
         <div className="space-y-[30px] lg:space-y-10">
@@ -69,9 +86,11 @@ export const JobCard: React.FC<JobCardProps> = (props) => {
           <JobRequirements requirements={requirements} />
         </div>
       </div>
-      <Button size="base" className="font-medium">
-        Apply Now
-      </Button>
+      <Reveal delay={0.4}>
+        <Button size="base" className="font-medium">
+          Apply Now
+        </Button>
+      </Reveal>
     </div>
   );
 };
